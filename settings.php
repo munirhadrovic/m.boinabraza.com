d2aa25319212880f362d8674a5d038cdd5c49c4c
anes89cok <anes89cok@gmail.com>
2015-05-04 15:38:52 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,18 @@
+<?php
+$adresa[0]="4025 William D Tate Ave"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Grapevine";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]="TX 76051"; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Grapevine";
+$telephone[0]= 8172519881;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="4025 William D Tate Ave"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Cincinnati";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]="OH 45202"; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Cincinnati";
+$telephone[1]=5134217111;// Broj koji se poziva na dugme CALL US
+
+$companynumber=2;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+$transitionefec="fade";
+?>
